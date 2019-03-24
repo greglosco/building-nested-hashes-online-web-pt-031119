7a1fc418720c86532091eded69c98739ac0c27d0
Gregory Losco <gtlosco@gmail.com>
2019-03-24 02:26:59 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,6 @@ def fourth_challenge
       {name: "Mercutio", age: "18", attitude: "hot-headed"
         }
       ]
-   }
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
